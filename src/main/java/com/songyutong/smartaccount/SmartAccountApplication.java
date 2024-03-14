cf8425ca946bb09240b196396f1f4000ab318b63
songyutong <1526058914@qq.com>
2024-03-14 16:36:35 +0800

    初始化项目

---
+++ src/main/java/com/songyutong/smartaccount/SmartAccountApplication.java
@@ -0,0 +1,13 @@
+package com.songyutong.smartaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/songyutong/smartaccount/SmartAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songyutong.smartaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
